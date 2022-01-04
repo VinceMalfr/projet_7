27aea690021d0a43cd536252208c8d17a6dd9476
VinceMalfr <88104379+VinceMalfr@users.noreply.github.com>
2022-01-04 18:16:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,9 @@ st.write("""
 	# My first app
 	Hello *world!*
 	""")
+st.title("Streamlit sliders")
+st subheader("Slider 1:")
+x = st.slider('a number between 0-100')
+
+st.write("Slider number:", x)
 	
