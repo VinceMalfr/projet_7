5ed14d203c56a94a32e356dfd4296fa4034a751c
VinceMalfr <88104379+VinceMalfr@users.noreply.github.com>
2022-01-18 18:01:13 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
+from ssl import Options
 import streamlit as st
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from PIL import Image
@@ -12,25 +14,23 @@ from urllib.request import urlopen
 import json
 import pickle 
 from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import KMeans
 
 
 
+## Agrandissement de la page ##
 st.set_page_config(layout='wide')
 
 ## Importations des bases de données 
-df = pd.read_csv('client_information.csv', index_col='SK_ID_CURR', encoding='utf-8')
-data = pd.read_csv("X_dash.csv", index_col='SK_ID_CURR', encoding='utf-8' )
-sample = pd.read_csv("client_information_sample.csv", index_col='SK_ID_CURR', encoding='utf-8' )
+df = pd.read_csv('client_information.csv', encoding='utf-8').set_index('SK_ID_CURR')
+data = pd.read_csv("X_dash.csv", encoding='utf-8' ).set_index('SK_ID_CURR')
+sample = pd.read_csv("client_information_sample.csv", encoding='utf-8' ).set_index('SK_ID_CURR')
 
 id_client = sample.index.values
 
-def credit_factors(model, patient,data):
 
-    explainer = shap.KernelExplainer(model, data)
-    shap_values = explainer.shap_values(patient)
-    shap.initjs()
-    return shap.force_plot(explainer.expected_value[1], shap_values[1], patient)
-
+#Importation de notre modèle  LGBM
+@st.cache
 def load_model():
     '''loading the trained model'''
     pickle_in = open('/Users/vincentMalfroy/Documents/GitHub/projet_7/LGBM.pkl', 'rb') 
@@ -39,6 +39,31 @@ def load_model():
 
     return clf
 
+# Entrainement d'un modèle kMeans 
+@st.cache
+def knn_training(sample):
+    knn = KMeans(n_clusters=2).fit(sample)
+    return knn 
+
+@st.cache(allow_output_mutation=True)
+def load_knn(sample):
+    knn = knn_training(sample)
+    return knn
+
+
+@st.cache
+def load_kmeans(sample, id, modele):
+    index = sample[sample.index == int(id)].index.values
+    index = index[0]
+    data_client = pd.DataFrame(sample.loc[sample.index, :])
+    df_neighbors = pd.DataFrame(knn.fit_predict(data_client), index=data_client.index)
+    df_neighbors = pd.concat([df_neighbors, data], axis=1)
+    return df_neighbors.iloc[:,2:].sample(5)
+
+
+
+#EXT_SOURCE_3 EXT_SOURCE_2 CREDIT_TERM AMT_GOODS_PRICE  EXT_SOURCE_1 AMT_CREDIT 
+# YEARS_BIRTH AMT_INCOME_TOTAL
 
 
 ################## SIDEBAR ######################
@@ -58,11 +83,9 @@ selection = st.sidebar.selectbox('Interprétation',['','Individuelle','Globale',
 ####################  CENTRAL ##########################
 header_container = st.container()
 with header_container:
-	#image= Image.open('logo_central.png')
-	#st.image(image,use_column_width=True)
 	st.markdown("<h1 style='text-align: center;color: white'>Simulation de prêt</h1>", unsafe_allow_html=True)
 	st.markdown("<h3 style='text-align: center;'>Obtenez une réponse imédiate</h3>", unsafe_allow_html=True)
-	
+
 
 st.write("**Information sur le client n°**",user_input)
 	####### Informations sur le client sélectionnée
@@ -71,60 +94,66 @@ client_age = info_client["YEARS_BIRTH"]
 client_education = info_client["NAME_EDUCATION_TYPE"]
 client_revenu = info_client["AMT_INCOME_TOTAL"]
 client_emploi = info_client['YEARS_EMPLOYED']
-#client_revenu = info_client[info_client['AMT_INCOME_TOTAL'] < 50000 ]
+client_contrat = info_client['NAME_CONTRACT_TYPE']
+client_enfant = info_client['CNT_CHILDREN']
+client_credit = info_client['AMT_CREDIT']
+client_relation = info_client['NAME_FAMILY_STATUS']
 
-col1, col2 = st.columns(2)
+
+container = st.container()
+all = st.checkbox("Selectionnez tous les critères")
+ 
+if all:
+    options = container.multiselect("Selectionnez un ou plusieurs critères:",
+         ['Genre', 'Age', 'Niveau scolaire', 'Revenu du client', "Nombre d'années travaillé", 'Type de contrat',
+		 "Nombre d'enfant", 'Montant du crédit', 'Statut maritale'],['Genre', 'Age', 'Niveau scolaire', 'Revenu du client', "Nombre d'années travaillé", 'Type de contrat',
+		 "Nombre d'enfant", 'Montant du crédit', 'Statut maritale'])
+else:
+    options =  container.multiselect("Selectionnez un ou plusieurs critères:",
+        ['Genre', 'Age', 'Niveau scolaire', 'Revenu du client', "Nombre d'années travaillé", 'Type de contrat', 
+		"Nombre d'enfant", 'Montant du crédit', 'Statut maritale'])
+
+
+col1, col2, col3 = st.columns(3)
 with col1:
-	## Genre
-	st.markdown("<u>Sexes:</u>", unsafe_allow_html=True)
-	st.text(client_genre)
+	if 'Genre' in options:
+		st.markdown("<u>Sexe:</u>", unsafe_allow_html=True)
+		st.text(client_genre)
 with col2:
-	## Age
-	st.markdown("<u>Age:</u>", unsafe_allow_html=True)
-	st.write(int(client_age), "years")
-
-check = st.checkbox("Plus d'informations")
-if check :
-	col3, col4, col5 = st.columns(3)
-	with col3:
-	## Education
+	if 'Age' in options:
+		st.markdown("<u>Age:</u>", unsafe_allow_html=True)
+		st.write(int(client_age), "years")
+with col3:
+	if 'Niveau scolaire' in options:
 		st.markdown("<u>Niveau scolaire:</u>", unsafe_allow_html=True)
 		st.write(client_education)
-	with col4:
-		## revenu
+
+col4, col5, col6 = st.columns(3)
+with col4:
+	if 'Revenu du client' in options:
 		st.markdown("<u>Revenu du client:</u>", unsafe_allow_html=True)
-		st.write(int(client_revenu), "$")
-	with col5:
-		## Nombre d'années employé
-		st.markdown("<u>Nomre d'années travaillé:</u>", unsafe_allow_html=True)
-		st.write(int(client_emploi))
-
-	###Figure 
-	col3, col4, col5 = st.columns(3)
-	with col3:
-		### Age du client dans la population totale 
-		fig, ax = plt.subplots(figsize=(10,5))
-		sns.histplot(data= data, x="YEARS_BIRTH", bins=10)
-		ax.axvline(int(info_client["YEARS_BIRTH"]), color='green', linestyle='--')
-		ax.set(title='Age du client dans la population totale', xlabel='Age', ylabel='')
-		st.pyplot(fig)
-
-	with col4:
-		### Revenu du client dans la population totale 
-		fig, ax = plt.subplots(figsize=(10,5))
-		sns.histplot(data= data, x="AMT_INCOME_TOTAL", bins=10)
-		ax.axvline(int(info_client["AMT_INCOME_TOTAL"]), color='green', linestyle='--')
-		ax.set(title='Revenu du client dans la population totale ', xlabel='revenu', ylabel='')
-		st.pyplot(fig)
-
-	with col5:
-
-		### Nombre d'années travaillé dans la population totale
-		fig, ax = plt.subplots(figsize=(10,5))
-		sns.histplot(data= data, x="YEARS_EMPLOYED", bins=30)
-		ax.axvline(int(info_client["YEARS_EMPLOYED"]), color='green', linestyle='--')
-		ax.set(title="Nombre d'années travaillé dans la population totale", xlabel='Année travaillés', ylabel='')
-		st.pyplot(fig)
+		st.write(int(client_revenu),"$")
+with col5:
+	if "Nombre d'années travaillé" in options:
+		st.markdown("<u>Nombre d'années travaillé:</u>", unsafe_allow_html=True)
+		st.write(int(client_emploi), "ans")
+with col6:
+	if 'Type de contrat' in options:
+		st.markdown("<u>Type de contrat :</u>", unsafe_allow_html=True)
+		st.write(client_contrat)
+col7, col8, col9 = st.columns(3)
+with col7:
+	if "Nombre d'enfant" in options:
+		st.markdown("<u>Nombre d'enfant :</u>", unsafe_allow_html=True)
+		st.write(int(client_enfant))
+with col8:
+	if 'Montant du crédit' in options:
+		st.markdown("<u>Montant du crédit :</u>", unsafe_allow_html=True)
+		st.write(int(client_credit),"$")
+with col9:
+	if 'Statut maritale' in options:
+		st.markdown("<u>Statut marital :</u>", unsafe_allow_html=True)
+		st.write(client_relation)
 
 
 	### Validation du pret
@@ -139,7 +168,7 @@ with st.spinner('Chargement du score client...'):
 	json_url = urlopen(API_url) 
 
 	API_data = json.loads(json_url.read())
-	prediction = API_data['Risque client en %']
+	prediction = API_data['risque_client']
 	st.write("**Risque de défaut client : **{:.0f} %".format(round(float(prediction), 2)))
 
 #Compute decision according to the best threshold 50% (it's just a guess)
@@ -152,51 +181,58 @@ else :
 	st.error(loanResult)
 
 
-    
-
 
 if selection == 'Individuelle':
 	st.markdown("<h2 style='text-align: center;'>Principaux indicateurs influençants le taux de risque</h2>", unsafe_allow_html=True)
 	shap.initjs()
 	X = sample[sample.index == user_input]
 
-	fig, ax = plt.subplots(figsize=(10, 10))
-	explainer = shap.TreeExplainer(load_model())
-	shap_values = explainer.shap_values(X)
-	shap.summary_plot(shap_values[0], X, plot_type ="bar", color_bar=True, plot_size=(5, 5))
-	st.pyplot(fig)
-
-
-	row_to_show = X
-	data_for_prediction = sample.iloc[row_to_show]
-
 	fig, ax = plt.subplots()
 	explainer = shap.TreeExplainer(load_model())
-	shap_values = explainer.shap_values(data_for_prediction)
-	shap.initjs()
-	shap.force_plot(explainer.expected_value[0], shap_values[0], data_for_prediction)
+	shap_values = explainer.shap_values(X)
+	shap.summary_plot(shap_values, X, max_display=15, plot_size=(5,5))
 	st.pyplot(fig)
+
+	fig, ax = plt.subplots(figsize=(5,5))
+	explainer = shap.TreeExplainer(load_model())
+	shap_values = explainer.shap_values(X)
+	shap.summary_plot(shap_values[0], X, max_display=15)
+	st.pyplot(fig)
+
 	
 
 
 if selection == 'Globale':
 	st.markdown("<h2 style='text-align: center;'>Principaux indicateurs influençants le taux de risque de la classe 0</h2>", unsafe_allow_html=True)
-	fig, ax = plt.subplots()
-	explainer = shap.TreeExplainer(load_model())
-	shap_values = explainer.shap_values(df)
-	shap.summary_plot(shap_values, df, max_display=10)
-	st.pyplot(fig)
+	st.image('le taux de risque_mean.png')
 
 	st.markdown("<h2 style='text-align: center;'>Principaux indicateurs influençants le taux de risque de la classe 0</h2>", unsafe_allow_html=True)
-	fig, ax = plt.subplots(figsize=(10, 10))
-	explainer = shap.TreeExplainer(load_model())
-	shap_values = explainer.shap_values(df)
-	shap.summary_plot(shap_values[0], df)
-	st.pyplot(fig)
+	st.image('le taux de risque.png')
 
 
-#if selection == 'Profils similaires':
+if selection == 'Profils similaires':
+	st.markdown("<h2 style='text-align: center;'>Comparaison des caractéristiques avec un echantillon de client au profils similaires</h2>", unsafe_allow_html=True)
+	
+	knn = load_knn(sample)
+	data_kmeans = load_kmeans(sample, user_input, knn)
+	#data_kmeans = data_kmeans.drop(columns=['Unnamed: 0'])
 
+	st.markdown("<u>Liste des 5 dossiers les plus proches du client :</u>", unsafe_allow_html=True)
+	st.dataframe(data_kmeans)
+	df_voisin = data_kmeans.reindex(columns=['EXT_SOURCE_3','EXT_SOURCE_2', 'CREDIT_TERM',
+                                    'AMT_GOODS_PRICE', 'EXT_SOURCE_1', 'AMT_CREDIT',
+                                    'YEARS_BIRTH', 'AMT_INCOME_TOTAL'])
+	
+	df_voisin = pd.DataFrame(df_voisin.mean(), columns=['Moyenne_profil_similaire'])
 
+	df_client = (data.loc[data.index == user_input])
+	df_client = df_client.reindex(columns=['EXT_SOURCE_3','EXT_SOURCE_2', 'CREDIT_TERM',
+                                    'AMT_GOODS_PRICE', 'EXT_SOURCE_1', 'AMT_CREDIT',
+                                    'YEARS_BIRTH', 'AMT_INCOME_TOTAL'])
+	df_client = df_client.T
+	#df_client = pd.DataFrame(df_client, columns=['Client'])
 
-
+	df_client = df_voisin.join(df_client)
+	
+	st.dataframe(df_client)
+	
